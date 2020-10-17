3c09f871f8d6769effcac5d15ba39456c45ce6c0
Derah Onuorah <donuorah1925@gmail.com>
2020-10-17 16:49:40 -0400

    Adding styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,8 @@ import React, {useState} from 'react'
 import * as faIcons from "react-icons/fa";
 import * as aiIcons from "react-icons/ai";
 import { Link } from 'react-router-dom';
+import {sidebarData, SidebarData} from './SidebarData';
+import './Navbar.css';
 
 function Navbar() {
     const[sidebar, setSidebar] = useState(false)
@@ -14,12 +16,22 @@ function Navbar() {
                     <faIcons.FaBars onClick={showSidebar} />
                 </Link>
                 <nav className={sidebar ? 'nav-menu active': 'nav-menu'}>
-                    <ul className='nav-menu-item'>
-                        <li className="navvr-toggle">  
+                    <ul className='nav-menu-items'>
+                        <li className="navbar-toggle">  
                             <Link to="#" className='menu-bars'>
                                <aiIcons.AiOutlineClose/>
                             </Link>
                         </li>
+                        {SidebarData.map((item, index)=> {
+                            return(
+                                <li key={index} className={item.cName}>
+                                    <Link to={item.path}>
+                                        {item.icon}
+                                        <span>{item.title}</span>
+                                    </Link>
+                                </li>
+                            )
+                        })}
                     </ul>
                 </nav>
             </div>
